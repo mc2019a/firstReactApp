54cb524e8c8082336d1a8864ae06398ae78a11e9
mc2019a <densabayev@gmail.com>
2024-06-29 20:35:08 +0300

    only JS code added

--- react_app1/src/App.js
+++ react_app1/src/App.js
@@ -1,29 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {createElement} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-            {new Date().getFullYear()}
-            {/*императивный код*/}
-        </p>
-      </header>
-    </div>
-  );
+    return createElement(
+        "div",
+        { className: "App" },
+        createElement(
+            "header",
+            { className: "App-header" },
+            createElement(
+                "img",
+                { className: "App-logo", src: logo, alt: "logo" },
+            ),
+            createElement(
+                "p",
+                {},
+                'Edit ',
+                createElement(
+                    "code",
+                    {},
+                    'src/App.js'
+                ),
+                ' and save to reload'
+            ),
+            createElement(
+                "a",
+                { className: "App-link", href:"https://reactjs.org", target: "_blank", rel: "noopener noreferrer" },
+                "Learn React",
+            ),
+            createElement(
+                "p",
+                {},
+                new Date().getFullYear()
+            )
+        )
+    )
 }
+
+
 
 export default App;
